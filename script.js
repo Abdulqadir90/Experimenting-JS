fae8128afeca90ad72eba0d26e6d31347618dea2
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-07-12 23:56:48 +0100

    trying the slice method

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-console.log("hello world");
+// console.log("hello world");
 
-let tweet = prompt("Write your new tweet:");
-let tweetCount = tweet.length;
-alert("You have written " + tweetCount + " characters, you have " + ( 140 - tweetCount ) + " characters remaining.");
+// let tweet = prompt("Write your new tweet:");
+// let tweetCount = tweet.length;
+// alert("You have written " + tweetCount + " characters, you have " + ( 140 - tweetCount ) + " characters remaining.");
+
+
+alert(prompt("Write your tweet:").slice(0,140)); 
+// same concept however this will only allow up to 140 characters
