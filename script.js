87d240a803055d5358cdff76d23591268cdcaa41
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-07-13 02:10:00 +0100

    made a var that calculates dog age in human years

--- script.js
+++ script.js
@@ -5,5 +5,11 @@
 // alert("You have written " + tweetCount + " characters, you have " + ( 140 - tweetCount ) + " characters remaining.");
 
 
-alert(prompt("Write your tweet:").slice(0,140)); 
+// alert(prompt("Write your tweet:").slice(0,140)); 
 // same concept however this will only allow up to 140 characters
+
+let dogAge = prompt("How old is your dog?");
+let humanAge = ((dogAge - 2) * 4) + 21;
+// brackets show order of calculations
+alert("Your dog is " + humanAge + " years old in human year!");
+
