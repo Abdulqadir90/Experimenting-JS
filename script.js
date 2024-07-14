1a601c6c3b68be52eafeb7767ebb0fa95c24543f
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-07-14 02:52:29 +0100

    figuring out how many days, weeks, months until 90 years

--- script.js
+++ script.js
@@ -6,10 +6,15 @@
 
 
 // alert(prompt("Write your tweet:").slice(0,140)); 
-// same concept however this will only allow up to 140 characters
+//same concept however this will only allow up to 140 characters
 
-let dogAge = prompt("How old is your dog?");
-let humanAge = ((dogAge - 2) * 4) + 21;
-// brackets show order of calculations
-alert("Your dog is " + humanAge + " years old in human year!");
+function lifeInWeeks(age) {
+    let yearsRemaining = 90 - age;
+    let days = yearsRemaining * 365; 
+    let weeks = yearsRemaining * 52;
+    console.log("you have " + days + " days");
 
+    
+}
+
+lifeInWeeks(10);
