76592b1d7197d321c75e6a25fd18a1c974f9d664
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-07-19 15:35:55 +0100

    sorted solution

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function lifeInWeeks(age) {
     let yearsRemaining = 90 - age;
     let days = yearsRemaining * 365; 
     let weeks = yearsRemaining * 52;
-    console.log("you have " + days + " days");
+    let months = yearsRemaining * 12
+    console.log("you have " + days +" days, " + weeks + " weeks, " + months + " months remaining");
 
     
 }
